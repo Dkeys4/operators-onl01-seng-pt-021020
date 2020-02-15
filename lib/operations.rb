da3affe181f70bbf2e21db563b2211023feb42a0
Dural Keys <duralinitaly@gmail.com>
2020-02-15 09:10:40 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,23 @@
 def unsafe?(speed)
+  if speed < 40  
+  return "true" 
+  elsif speed > 60 
+  return "true"
+  else 40 speed >= && speed <= 60
+  return false 
 
 end
-
-
+end
 
 def not_safe?(speed)
+  if speed > 40? yes it is : no it is not 
+    return true
+  elsif speed 60 > ? yes it is : no it is not  
+  return true
+else
+  return false 
 	
 end
-	
+end	
 
 
